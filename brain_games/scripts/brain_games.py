72475c280a16e08d166f68ab9950a552786a8269
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-08-28 17:47:12 +0500

    modified brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+# Start game: brain_games
+
+
+from brain_games.games import cli
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    cli.welcome_user()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
