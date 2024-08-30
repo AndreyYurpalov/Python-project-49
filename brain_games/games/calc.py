d5c5ee188d7c6deb7479eb2ecc46d0841fd0fe86
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-08-30 16:16:27 +0500

    modified calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,7 +6,7 @@ dic_res['question'] = 'What is the result of the expression?'
 
 
 def game_cycle():
-    sp_arguments = [random.randint(1, 15) for _ in range(2)]
+    sp_arguments = [random.randint(1, 20) for _ in range(2)]
     actions = ('+', '-', '*')
     action = random.choice(actions)
     if action == '+':
@@ -16,6 +16,7 @@ def game_cycle():
     else:
         correct_answer = sp_arguments[0] * sp_arguments[1]
     dic_res['correct_answer'] = str(correct_answer)
-    number = f'{sp_arguments[0]} {action} {sp_arguments[1]}'
-    dic_res['number'] = number
+    dic_res['number1'] = sp_arguments[0]
+    dic_res['number2'] = action
+    dic_res['number3'] = sp_arguments[1]
     return dic_res
