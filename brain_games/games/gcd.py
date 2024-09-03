0396116e117d905bc5c2096e3552bdb424bcc324
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-03 18:13:41 +0500

    modified gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,19 +1,14 @@
 import random
+import math
+from brain_games.games_engine import engine, QUESTION_GCD
 
 
-dic_res = dict()
-dic_res['question'] = 'Find the greatest common divisor of given numbers.'
+def game_cycle_gcd():
+    num1, num2 = random.randint(1, 20), random.randint(1, 20)
+    correct_answer = math.gcd(num1, num2)
+    question = f'{num1}  {num2}'
+    return question, str(correct_answer)
 
 
-def game_cycle():
-    sp_arguments = [random.randint(1, 12) for _ in range(2)]
-    sp_arguments.sort()
-    answer = [sp_arguments[0] // (i + 1) for i in range(sp_arguments[0])
-              if sp_arguments[0] % (i + 1) == 0]
-    control_answer = [sp_arguments[1] // answer[i] for i in range(len(answer))
-                      if sp_arguments[1] % answer[i] == 0]
-    correct_answer = sp_arguments[1] // min(control_answer)
-    dic_res['correct_answer'] = str(correct_answer)
-    dic_res['number1'] = sp_arguments[0]
-    dic_res['number2'] = sp_arguments[1]
-    return dic_res
+def start_game_gcd():
+    engine(game_cycle_gcd, QUESTION_GCD)
