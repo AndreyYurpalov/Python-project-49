2a020ef53816881d232a48bfe4eba09beb17ea17
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-04 12:24:48 +0500

    modified gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,7 +6,7 @@ from brain_games.games_engine import engine, QUESTION_GCD
 def game_cycle_gcd():
     num1, num2 = random.randint(1, 20), random.randint(1, 20)
     correct_answer = math.gcd(num1, num2)
-    question = f'{num1}  {num2}'
+    question = f'{num1} {num2}'
     return question, str(correct_answer)
 
 
