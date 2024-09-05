304cda4751ce2214815f0b42c54bc00548f32ae9
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:55:32 +0500

    modified brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.calc import start_game_calc
+from brain_games.games import calc
+from brain_games.games_engine import engine
 
 
 def main():
-    start_game_calc()
+    engine(calc.get_arguments_of_expression_and_answer, calc.QUESTION_OF_GAME)
 
 
 if __name__ == '__main__':
