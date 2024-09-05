649282a6a4612aef5317bc65b1f18d64a592d2be
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:57:08 +0500

    modified brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.progression import start_game_progression
+from brain_games.games import progression
+from brain_games.games_engine import engine
 
 
 def main():
-    start_game_progression()
+    engine(progression.get_progression_whit_missing_number_and_answer,
+           progression.QUESTION_OF_GAME)
 
 
 if __name__ == '__main__':
