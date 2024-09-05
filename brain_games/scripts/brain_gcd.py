6b6e76d2d793d34cb79a605cf03f4e3296d164c8
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:56:12 +0500

    modified brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.gcd import start_game_gcd
+from brain_games.games import gcd
+from brain_games.games_engine import engine
 
 
 def main():
-    start_game_gcd()
+    engine(gcd.get_numbers_and_answer, gcd.QUESTION_OF_GAME)
 
 
 if __name__ == '__main__':
