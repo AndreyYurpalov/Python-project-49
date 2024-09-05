cb33d9aad95ef7cd1f3d33f0d37b99e43199c19e
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:56:45 +0500

    modified brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.prime import start_game_prime
+from brain_games.games import prime
+from brain_games.games_engine import engine
 
 
 def main():
-    start_game_prime()
+    engine(prime.get_number_and_answer, prime.QUESTION_OF_GAME)
 
 
 if __name__ == '__main__':
