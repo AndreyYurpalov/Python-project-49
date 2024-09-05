f4549488ce45b1e97fd6730c9afffe2f8c6b3361
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:53:19 +0500

    modified even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,16 +1,18 @@
+# ---   The goal of the game is to answer the question   ---
+# ---                  Is the number even?               ---
+
+
 import random
-from brain_games.games_engine import engine, QUESTION_EVEN
+
+
+QUESTION_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(num):
     return num % 2 == 0
 
 
-def game_cycle_even():
+def get_random_num_and_answer():
     question = random.randint(1, 100)
     correct_answer = 'yes' if is_even(question) else 'no'
     return str(question), str(correct_answer)
-
-
-def start_game_even():
-    engine(game_cycle_even, QUESTION_EVEN)
