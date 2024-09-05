f48e7430772c0fb103925732593a2cd29a88cf35
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:54:03 +0500

    modified prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,14 +1,21 @@
+# ---   The goal of the game is to answer the question   ---
+# ---                 Is the number prime?               ---
+
+
 import random
-from brain_games.games_engine import engine, QUESTION_PRIME
 
 
-def game_cycle_prime():
+QUESTION_OF_GAME = ('Answer "yes" if given number is prime. '
+                    'Otherwise answer "no".')
+
+
+def is_number_prime(number):
+    divisions = [i for i in range(2, number - 1) if number % i == 0]
+    return len(divisions) < 1
+
+
+def get_number_and_answer():
     len_sp = 100
     question = random.randint(2, len_sp)
-    list_div = [i for i in range(2, question - 1) if question % i == 0]
-    correct_answer = 'yes' if len(list_div) < 1 else 'no'
+    correct_answer = 'yes' if is_number_prime(question) else 'no'
     return question, str(correct_answer)
-
-
-def start_game_prime():
-    engine(game_cycle_prime, QUESTION_PRIME)
