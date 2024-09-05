b1420800efc5f989b0d0e66a91751bfd5b76e0ba
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:55:48 +0500

    modified brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.even import start_game_even
+from brain_games.games import even
+from brain_games.games_engine import engine
 
 
 def main():
-    start_game_even()
+    engine(even.get_random_num_and_answer, even.QUESTION_OF_GAME)
 
 
 if __name__ == '__main__':
