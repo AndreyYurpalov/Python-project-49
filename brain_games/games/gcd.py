61f9f78e5d73118ae9570e599da56b874b40dc5f
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-05 15:53:42 +0500

    modified gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,14 +1,16 @@
+# ---   The goal of the game is to answer the question   ---
+# ---  Find the greatest common divisor of given numbers. ---
+
+
 import random
 import math
-from brain_games.games_engine import engine, QUESTION_GCD
 
 
-def game_cycle_gcd():
+QUESTION_OF_GAME = 'Find the greatest common divisor of given numbers.'
+
+
+def get_numbers_and_answer():
     num1, num2 = random.randint(1, 20), random.randint(1, 20)
     correct_answer = math.gcd(num1, num2)
     question = f'{num1} {num2}'
     return question, str(correct_answer)
-
-
-def start_game_gcd():
-    engine(game_cycle_gcd, QUESTION_GCD)
