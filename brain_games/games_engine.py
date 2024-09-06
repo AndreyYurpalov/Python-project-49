699056fa4a4a2763d8efe48bbf9f31804546362f
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-06 16:38:29 +0500

    modified games_engine.py

--- brain_games/games_engine.py
+++ brain_games/games_engine.py
@@ -5,11 +5,11 @@ from brain_games.games.cli import welcome_user
 ATTEMPT = 3
 
 
-def engine(game, quest):
+def engine(game_modul):
     name = welcome_user()
-    print(quest)
+    print(game_modul.QUESTION_OF_GAME)
     for _ in range(ATTEMPT):
-        question, correct_answer = game()
+        question, correct_answer = game_modul.get_question_and_correct_answer()
         print(f'Question: {question}')
         your_answer = prompt.string("Your answer: ")
         if str(your_answer) == str(correct_answer):
